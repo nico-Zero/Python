2f89abdf9e73cb8a5ae2992ab8e6352a976f57ff
nico-zero <zandaxheart955@gmail.com>
2022-10-17 00:36:51 +0530

    updating python folder

--- New_Shit__/Angela_Yu__/Day_31 #/main.py
+++ New_Shit__/Angela_Yu__/Day_31 #/main.py
@@ -5,13 +5,13 @@ from random import choice
 # -------------------------------------------------------VARIABLES---------------------------------------------------------------
 # Display ->
 display = customtkinter.CTk()
-new_x = None
 
 with open("New_Shit__/Angela_Yu__/Day_31 #/data/fr_en.csv") as file:
     data = [(i.split(",")[0], i.split(",")[1][:-1]) for i in file.readlines()]
-    x = choice(data)
-    france = f"France:\n{x[0]}"
-    english = f"English:\n{x[1]}"
+
+    tuple = choice(data)
+    france = f"France:\n{tuple[0]}"
+    english = f"English:\n{tuple[1]}"
 
 STATE = ["Dark", "Light"]
 BACKGROUND_COLOR = "#B1DDC6"
@@ -30,23 +30,42 @@ display.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
 # -------------------------------------------------------FUNCTIONS---------------------------------------------------------------
 def new_word():
-    global france, english,new_x
+    global france, english, tuple
 
-    new_x = choice(data)
-    france = f"France:\n{new_x[0]}"
-    english = f"English:\n{new_x[1]}"
+    tuple = choice(data)
+    france = f"France:\n{tuple[0]}"
+    english = f"English:\n{tuple[1]}"
 
 
 def turn_card(sec=3):
-    print(new_x)
     if sec != 0:
-        flash_card.itemconfig(card, image=CARD_FRONT)
-        flash_card.itemconfig(card_text, text=france)
+
         display.after(1000, turn_card, sec - 1)
     else:
         flash_card.itemconfig(card, image=CARD_BACK)
         flash_card.itemconfig(card_text, text=english)
+
+        wrong_button.configure(state=customtkinter.NORMAL)
+        right_button.configure(state=customtkinter.NORMAL)
         new_word()
+
+def start():
+    flash_card.itemconfig(card, image=CARD_FRONT)
+    flash_card.itemconfig(card_text, text=france)
+    wrong_button.configure(state=customtkinter.DISABLED)
+    right_button.configure(state=customtkinter.DISABLED)
+    turn_card()
+
+def right():
+    print("Already know",tuple)
+    data.remove(tuple)
+    start()
+
+
+def wrong():
+    print("Don't know",tuple)
+    start()
+
 
 # -------------------------------------------------------UI---------------------------------------------------------------
 
@@ -56,7 +75,9 @@ flash_card = customtkinter.CTkCanvas(
     width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 )
 card = flash_card.create_image(400, 263, image=CARD_FRONT)
-card_text = flash_card.create_text(400, 263, text=france,justify="center",font=("Courier",20))
+card_text = flash_card.create_text(
+    400, 263, text=france, justify="center", font=("Courier", 20)
+)
 flash_card.place(x=50, y=0)
 
 # -------------------------------------------------------LABEL---------------------------------------------------------------
@@ -70,7 +91,7 @@ wrong_button = customtkinter.CTkButton(
     highlightthickness=0,
     border=False,
     fg_color=BACKGROUND_COLOR,
-    command=turn_card,
+    command=wrong,
 )
 right_button = customtkinter.CTkButton(
     text="",
@@ -79,9 +100,11 @@ right_button = customtkinter.CTkButton(
     highlightthickness=0,
     border=False,
     fg_color=BACKGROUND_COLOR,
-    command=turn_card,
+    command=right,
 )
+
 wrong_button.place(x=200, y=535)
 right_button.place(x=600, y=535)
 
+start()
 display.mainloop()
