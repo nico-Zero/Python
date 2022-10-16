bf1c1fce508011fd9a0d70e7bb2a5179bcbd4488
nico-zero <zandaxheart955@gmail.com>
2022-10-17 01:40:21 +0530

    updating python folder

--- New_Shit__/Angela_Yu__/Day_31 #Learning_game/main.py
+++ New_Shit__/Angela_Yu__/Day_31 #Learning_game/main.py
@@ -10,9 +10,6 @@ display = customtkinter.CTk()
 data = read_csv("New_Shit__/Angela_Yu__/Day_31 #Learning_game/data/fr_en.csv")
 data = data.to_dict(orient="records")
 
-chosen_one = choice(data)
-france = chosen_one["France"].lower()
-english = chosen_one["English"].lower()
 
 STATE = ["Dark", "Light"]
 BACKGROUND_COLOR = "#B1DDC6"
@@ -43,27 +40,20 @@ def new_word():
     english = chosen_one["English"].lower()
 
 
-def turn_card(sec=3):
-    if sec != 0:
-        display.after(1000, turn_card, sec - 1)
-    else:
-        flash_card.itemconfig(card, image=CARD_BACK)
-        flash_card.itemconfig(card_title, text="English:")
-        flash_card.itemconfig(card_word, text=english)
-
-        wrong_button.configure(state=customtkinter.NORMAL)
-        right_button.configure(state=customtkinter.NORMAL)
-        new_word()
-
-
 def start():
+    global turn
+    display.after_cancel(turn)
+    new_word()
     flash_card.itemconfig(card, image=CARD_FRONT)
     flash_card.itemconfig(card_title, text="France:")
     flash_card.itemconfig(card_word, text=france)
+    turn = display.after(3000, turn_card)
 
-    wrong_button.configure(state=customtkinter.DISABLED)
-    right_button.configure(state=customtkinter.DISABLED)
-    turn_card()
+
+def turn_card():
+    flash_card.itemconfig(card, image=CARD_BACK)
+    flash_card.itemconfig(card_title, text="English:")
+    flash_card.itemconfig(card_word, text=english)
 
 
 def right():
@@ -119,5 +109,6 @@ right_button = customtkinter.CTkButton(
 wrong_button.place(x=200, y=535)
 right_button.place(x=600, y=535)
 
+turn = display.after(3000, turn_card)
 start()
 display.mainloop()
