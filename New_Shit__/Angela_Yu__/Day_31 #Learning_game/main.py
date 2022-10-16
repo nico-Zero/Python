1343377d1ec37463bc7cb5918e48f216216d991b
nico-zero <zandaxheart955@gmail.com>
2022-10-17 00:51:27 +0530

    updating python folder

--- New_Shit__/Angela_Yu__/Day_31 #Learning_game/main.py
+++ New_Shit__/Angela_Yu__/Day_31 #Learning_game/main.py
@@ -10,14 +10,18 @@ with open("New_Shit__/Angela_Yu__/Day_31 #Learning_game/data/fr_en.csv") as file
     data = [(i.split(",")[0], i.split(",")[1][:-1]) for i in file.readlines()]
 
     tuple = choice(data)
-    france = f"France:\n{tuple[0]}"
-    english = f"English:\n{tuple[1]}"
+    france = tuple[0].lower()
+    english = tuple[1].lower()
 
 STATE = ["Dark", "Light"]
 BACKGROUND_COLOR = "#B1DDC6"
 
-CARD_FRONT = PhotoImage(file="New_Shit__/Angela_Yu__/Day_31 #Learning_game/images/card_front.png")
-CARD_BACK = PhotoImage(file="New_Shit__/Angela_Yu__/Day_31 #Learning_game/images/card_back.png")
+CARD_FRONT = PhotoImage(
+    file="New_Shit__/Angela_Yu__/Day_31 #Learning_game/images/card_front.png"
+)
+CARD_BACK = PhotoImage(
+    file="New_Shit__/Angela_Yu__/Day_31 #Learning_game/images/card_back.png"
+)
 
 RIGHT = PhotoImage(file="New_Shit__/Angela_Yu__/Day_31 #Learning_game/images/right.png")
 WRONG = PhotoImage(file="New_Shit__/Angela_Yu__/Day_31 #Learning_game/images/wrong.png")
@@ -25,6 +29,7 @@ WRONG = PhotoImage(file="New_Shit__/Angela_Yu__/Day_31 #Learning_game/images/wro
 
 # -------------------------------------------------------CUSTOMTKINTER-SETUPS---------------------------------------------------------------
 customtkinter.set_appearance_mode(STATE := STATE[0])
+display.title("Flash-Game")
 display.minsize(width=1000, height=750)
 display.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
@@ -33,8 +38,8 @@ def new_word():
     global france, english, tuple
 
     tuple = choice(data)
-    france = f"France:\n{tuple[0]}"
-    english = f"English:\n{tuple[1]}"
+    france = tuple[0].lower()
+    english = tuple[1].lower()
 
 
 def turn_card(sec=3):
@@ -43,27 +48,32 @@ def turn_card(sec=3):
         display.after(1000, turn_card, sec - 1)
     else:
         flash_card.itemconfig(card, image=CARD_BACK)
-        flash_card.itemconfig(card_text, text=english)
+        flash_card.itemconfig(card_title, text="English:")
+        flash_card.itemconfig(card_word, text=english)
 
         wrong_button.configure(state=customtkinter.NORMAL)
         right_button.configure(state=customtkinter.NORMAL)
         new_word()
 
+
 def start():
     flash_card.itemconfig(card, image=CARD_FRONT)
-    flash_card.itemconfig(card_text, text=france)
+    flash_card.itemconfig(card_title, text="France:")
+    flash_card.itemconfig(card_word, text=france)
+
     wrong_button.configure(state=customtkinter.DISABLED)
     right_button.configure(state=customtkinter.DISABLED)
     turn_card()
 
+
 def right():
-    print("Already know",tuple)
+    print("Already know", tuple)
     data.remove(tuple)
     start()
 
 
 def wrong():
-    print("Don't know",tuple)
+    print("Don't know", tuple)
     start()
 
 
@@ -75,8 +85,11 @@ flash_card = customtkinter.CTkCanvas(
     width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 )
 card = flash_card.create_image(400, 263, image=CARD_FRONT)
-card_text = flash_card.create_text(
-    400, 263, text=france, justify="center", font=("Courier", 20)
+card_title = flash_card.create_text(
+    400, 150, justify="center", font=("Courier", 40, "italic")
+)
+card_word = flash_card.create_text(
+    400, 263, justify="center", font=("Courier", 60, "bold")
 )
 flash_card.place(x=50, y=0)
 
