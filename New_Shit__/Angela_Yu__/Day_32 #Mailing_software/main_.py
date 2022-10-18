a2ffb0f54b4ae7c7892d369912d8cd28cb250dff
zero <zero@zero.zandaxheart@gmail.com>
2022-10-18 20:06:04 +0530

    updating python file

--- New_Shit__/Angela_Yu__/Day_32 #Mailing_software/main_.py
+++ New_Shit__/Angela_Yu__/Day_32 #Mailing_software/main_.py
@@ -1,26 +1,40 @@
 from datetime import datetime
 import smtplib
 from random import choice
+from email.message import EmailMessage
+import ssl
 
-my_email = "nicozero674@yahoo.com"
-friend_email = "zandaxheart955@gmail.com"
-my_pass = "FuckYouIn_69"
+
+my_email = "ZandaXheart955@gmail.com"
+friend_email = "nicozero674@yahoo.com"
+my_pass = "imlhjktowhazcccc"
 
 now = datetime.now()
 week_day = now.weekday()
 
 
-if week_day == 0:
-    with open("New_Shit__/Angela_Yu__/Day_32 #Mailing_software/quotes.txt") as file:
-        quot = file.readlines()
-        random_quot = choice(quot)
-        print(random_quot)
+if week_day == 1:
+    # with open("New_Shit__/Angela_Yu__/Day_32 #Mailing_software/quotes.txt") as file:
+    #     quot = file.readlines()
+    #     random_quot = choice(quot)
+    #     print(random_quot)
+
+    subject = "GoodMorning"
+    body = "random_quot"
+
+    em = EmailMessage()
+    em["From"] = my_email
+    em["To"] = friend_email
+    em["Subject"] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
 
     # Port for SSL: 465 Port for TLS/STARTTLS: 587
-    with smtplib.SMTP_SSL("smtp.mail.yahoo.com", 465) as connection:
-        connection.login(user=my_email, password=my_pass)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as connection:
+        connection.login(my_email, my_pass)
         connection.sendmail(
-            from_addr=my_email,
-            to_addrs=friend_email,
-            msg=f"Subject:Happy GoodMorning\n\n{random_quot}",
+            my_email,
+            friend_email,
+            em.as_string(),
         )
