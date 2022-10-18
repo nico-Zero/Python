87e92ec51c0f0cefd8a23a28a9995522a2b308f7
nico-zero <zandaxheart955@gmail.com>
2022-10-19 01:42:31 +0530

    updating python folder

--- New_Shit__/Angela_Yu__/Day_33 #API/api.py
+++ New_Shit__/Angela_Yu__/Day_33 #API/api.py
@@ -1,1 +1,8 @@
 # http://api.open-notify.org/iss-now.json
+import requests
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+if response.status_code != 200:
+    print("Error")
+else:
+    response = response.json()
+    print(response)
