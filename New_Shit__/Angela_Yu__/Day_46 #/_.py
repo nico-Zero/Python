2f29208e0023f27cf5c3239ff0cc7892eef6e0a0
nico-zero <zandaxheart955@gmail.com>
2022-11-12 00:02:45 +0530

    updating python folder

--- New_Shit__/Angela_Yu__/Day_46 #/_.py
+++ New_Shit__/Angela_Yu__/Day_46 #/_.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+from functools import reduce
+
 
 Url = "https://www.billboard.com/charts/hot-100/"
 date = input("Enter a date in Y-M-D:- ") or ""
@@ -11,12 +13,15 @@ Url = Url + date
 data = requests.get(url=Url)
 data.raise_for_status()
 data = data.text
-with open("music.html", "w") as file:
-    file.write(data)
 
 soup = BeautifulSoup(data, "html.parser")
-id_ = "title-of-a-story"
-clas = "c-title  a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 u-font-size-23@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-245 u-max-width-230@tablet-only u-letter-spacing-0028@tablet"
+clas = "lrv-u-width-100p"
 
-h3 = soup.find_all(name="h3", id=id_)
-print(len(h3))
+li = soup.find_all(name="li", class_=clas)
+
+
+with open("music.html", "w") as file:
+    file.write(reduce(lambda x, y: str(x) + str(y), li))
+
+print(len(li))
+print(li)
