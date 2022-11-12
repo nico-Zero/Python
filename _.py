ea2c3eceb2d2d91ee15bbd6b9bea0883f51c705d
nico-zero <zandaxheart955@gmail.com>
2022-11-12 14:46:29 +0530

    updating python folder

--- _.py
+++ _.py
@@ -1,12 +1,14 @@
 import requests
 
-URL = "https://www.google.co.in/"
+url = "https://sms77io.p.rapidapi.com/sms"
 
-data = requests.get(url=URL)
-data.raise_for_status()
-data = data.text
+payload = "to=%2B917247477955&p=JT5qe4eP6u7Z88wX0UUhtMaWsTTKGZEGk9ldruv36wClh6aqnVqyg9Eew0KJwmwwYM6KmMQE21LwJJ3k5PStA2UjhQ&text=Dear%20customer.%20We%20want%20to%20say%20thanks%20for%20your%20trust.%20Use%20code%20MINUS10%20for%2010%20%25%20discount%20on%20your%20next%20order!&json=1"
+headers = {
+    "content-type": "application/x-www-form-urlencoded",
+    "X-RapidAPI-Key": "bbaf8bc7dcmsh888f9b9b549d0b1p1642cdjsnfc4e5de2d813",
+    "X-RapidAPI-Host": "sms77io.p.rapidapi.com",
+}
 
-with open("data.html","w") as file:
-    file.write(data)
+response = requests.request("POST", url, data=payload, headers=headers)
 
-print(data)
+print(response.text)
