b2f73605d944cb7c85289291d75aa7fdc284a40d
nico-zero <zandaxheart955@gmail.com>
2022-11-14 00:58:30 +0530

    updating python folder

---
+++ kivy/main.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+
+class BoxLayoutExample(BoxLayout):
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = "vertical"
+        
+    #     b1 = Button(text="A")
+    #     b2 = Button(text="B")
+    #     b3 = Button(text="C")
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
+
+    pass
+
+
+class MainWidget(Widget):
+    pass
+
+
+class TheLab(App):
+    pass
+
+
+TheLab().run()
