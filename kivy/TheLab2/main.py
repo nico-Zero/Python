8b409f4ef17bb479f064af5b749281b1c336a610
nico-zero <zandaxheart955@gmail.com>
2022-11-16 02:36:57 +0530

    updating python folder

--- kivy/TheLab2/main.py
+++ kivy/TheLab2/main.py
@@ -1,19 +1,11 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.metrics import dp
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.stacklayout import StackLayout
 from kivy.properties import StringProperty, BooleanProperty
 
 
 class WidgetsExample(GridLayout):
     number_x = 1
     click_count = 0
-    button_state = BooleanProperty(False)
-    slider_state = BooleanProperty(False)
     display = StringProperty(str(click_count))
 
     def on_button_click(self):
@@ -23,21 +15,13 @@ class WidgetsExample(GridLayout):
 
     def change_state(self, widget):
         print("Button state:- ", widget.state)
-        if self.button_state:
-            self.button_state = False
-            widget.text = "OFF"
-        else:
-            self.button_state = True
+        if widget.state == "down":
             widget.text = "ON"
+        else:
+            widget.text = "OFF"
 
     def change_way(self, widget):
         print("Switch:- ", widget.active)
-        self.number_x *= -1
-        self.slider_state = widget.active
-
-    def update_value(self, widget):
-        self.display = str(round(widget.value))
-        self.click_count = round(widget.value)
 
 
 class MyApp(App):
