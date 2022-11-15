8c91b5821d8b911d056d40838ac206c0967ccf1d
nico-zero <zandaxheart955@gmail.com>
2022-11-15 22:45:34 +0530

    updating python folder

--- kivy/TheLab2/main.py
+++ kivy/TheLab2/main.py
@@ -10,27 +10,34 @@ from kivy.properties import StringProperty, BooleanProperty
 
 
 class WidgetsExample(GridLayout):
-    hello = StringProperty("Hello!")
-    state = BooleanProperty(True)
-    click_count = 0
     number_x = 1
+    click_count = 0
+    button_state = BooleanProperty(False)
+    slider_state = BooleanProperty(False)
+    display = StringProperty(str(click_count))
 
     def on_button_click(self):
         self.click_count += self.number_x
         print(f"Button clicked for {self.click_count} time")
-        self.hello = str(self.click_count)
+        self.display = str(self.click_count)
 
     def change_state(self, widget):
-        print("Button State:- ", widget.state)
-        if self.state:
-            self.state = False
-            widget.text = "ON"
-        else:
-            self.state = True
+        print("Button state:- ", widget.state)
+        if self.button_state:
+            self.button_state = False
             widget.text = "OFF"
+        else:
+            self.button_state = True
+            widget.text = "ON"
 
-    def change_way(self):
+    def change_way(self, widget):
+        print("Switch:- ", widget.active)
         self.number_x *= -1
+        self.slider_state = widget.active
+
+    def update_value(self, widget):
+        self.display = str(round(widget.value))
+        self.click_count = round(widget.value)
 
 
 class MyApp(App):
