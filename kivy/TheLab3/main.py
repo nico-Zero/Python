30f075344bede716d29d51982a166f3fc4b5f02b
zero <zero@zero.zandaxheart955@gmail.com>
2022-11-16 10:29:58 +0530

    updating Python folder

---
+++ kivy/TheLab3/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.uix.widget import Widget
+
+
+class CanvasExample1(Widget):
+    pass
+
+
+class MyApp(App):
+    pass
+
+
+MyApp().run()
