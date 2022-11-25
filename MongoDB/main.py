4c0eacddf7e526566fce8cdae8005d1b532582dc
nico-zero <zandaxheart@gmail.com>
2022-11-25 22:51:44 +0530

    updating python folder

--- MongoDB/main.py
+++ MongoDB/main.py
@@ -1,2 +1,13 @@
+from pymongo import MongoClient
 
+# my_client = MongoClient("localhost",27017)
+my_client = MongoClient("mongodb://localhost:27017")
+print(my_client,"\n")
 
+# databases = my_client.list_database_names()
+# print(databases)
+
+# my_store_db = my_client.my_store
+my_store_db = my_client["my_store"]
+
+print(my_store_db)
