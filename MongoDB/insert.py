eacb55a595731081eff1bb9b69a81d65536f6dc3
nico-zero <zandaxheart@gmail.com>
2022-11-26 02:58:07 +0530

    updating python folder

--- MongoDB/insert.py
+++ MongoDB/insert.py
@@ -1,10 +1,19 @@
 import pymongo
 from pymongo import MongoClient
+from faker import Faker
 
+f_data = Faker()
 
 client = MongoClient("mongodb://localhost:27017")
 my_products = client["my_store"]["products"]
 
-bag = {"name": "Gucci", "price": 50000}
+# bag = [
+#     {"name": f_data.factories(), "price": f_data.random_number()}
+#     for _ in range(int(input("Enter the number of entries:- ")))
+# ]
 
-print(my_products.insert_one(bag))
+# print(my_products.insert_many(bag))
+
+# my_products.delete_many({})
+
+print(f_data.job())
