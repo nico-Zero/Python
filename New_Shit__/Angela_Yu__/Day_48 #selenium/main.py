e699c09fb5d2b1095411124c1aa102fa8bbfa88b
nico-zero <zandaxheart@gmail.com>
2022-12-10 18:35:12 +0530

    updating...

--- New_Shit__/Angela_Yu__/Day_48 #selenium/main.py
+++ New_Shit__/Angela_Yu__/Day_48 #selenium/main.py
@@ -20,8 +20,8 @@ driver.get("https://www.python.org/")
 # python_logo = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
 # print(python_logo.get_attribute("href"))
 
-# submit_bug = driver.find_element(By.XPATH,"/html/body/div/footer/div[2]/div/ul/li[3]/a")
-# print(submit_bug.get_attribute("href"))
+submit_bug = driver.find_element(By.XPATH,"/html/body/div/footer/div[2]/div/ul/li[3]/a")
+print(submit_bug.get_attribute("href"))
 
 
 driver.quit()
