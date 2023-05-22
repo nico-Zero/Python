7bf8c120fd936c95676a1b43f9449f876622dd0f
Nico-Zero <zandaxheart955@gmail.com>
2023-05-22 11:22:43 +0530

    updating...

--- New_Shit__/Angela_Yu__/Day_45_Beautiful_soup/beautiful_soup.py
+++ New_Shit__/Angela_Yu__/Day_45_Beautiful_soup/beautiful_soup.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
+import lxml
+# import bs4
 
 # import lxml -> If the html.parser does not work ro give an error about the parser.
-
+# and just lxml in place fo html.parser in line 11.
 
 with open("example/index.html") as f:
     contents = f.read()
@@ -13,11 +15,12 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.title.string)
 # print(soup.prettify())
 # print(soup.p)
+# print(soup.a)
 
-all_a = soup.find_all(name="a")
-print(all_a)
-for tag in all_a:
-    print(tag.getText(), ":- ", tag.get("href"))
+# all_a = soup.find_all(name="a")
+# print(all_a)
+# for tag in all_a:
+#     print(tag.getText(), ":- ", tag.get("href"))
 
-# url = soup.select_one(selector="table p a")
-# print(url)
+url = soup.select_one(selector="table p a")
+eval("print(url.name)")
