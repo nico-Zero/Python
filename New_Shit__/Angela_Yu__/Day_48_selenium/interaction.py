abd54d88118bc08a21a5e67a592fd52d5bf6a88c
Nico-Zero <zandaxheart955@gmail.com>
2023-05-24 17:04:22 +0530

    updating...

--- New_Shit__/Angela_Yu__/Day_48_selenium/interaction.py
+++ New_Shit__/Angela_Yu__/Day_48_selenium/interaction.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
@@ -11,16 +12,17 @@ driver = webdriver.Chrome(executable_path=webdriver_location, options=chrome_opt
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-# number = driver.find_element(By.CSS_SELECTOR,"#articlecount > a:nth-child(1)" )
+# number = driver.find_element(By.CSS_SELECTOR, "#articlecount > a:nth-child(1)")
 # number.click()
 
-# int_num = int(number.text.replace(",",""))
-
+# int_num = int(number.text.replace(",", ""))
 # print(int_num)
 
-# history = driver.find_element(By.LINK_TEXT,"View history")
-# history.click()
+history = driver.find_element(By.LINK_TEXT, "View history")
+history.click()
 
-search_box = driver.find_element(By.NAME,"search")
-search_box.send_keys("python")
-search_box.send_keys(Keys.ENTER)
+# search_box = driver.find_element(By.NAME, "search")
+# search_box.send_keys("python")
+# search_box.send_keys(Keys.ENTER)
+
+# driver.quit()
