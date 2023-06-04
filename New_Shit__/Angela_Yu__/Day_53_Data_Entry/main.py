bf8a27438a5893a456c38cc4218e594e8eb96314
Nico-Zero <zandaxheart955@gmail.com>
2023-06-05 04:07:49 +0530

    updating...

--- New_Shit__/Angela_Yu__/Day_53_Data_Entry/main.py
+++ New_Shit__/Angela_Yu__/Day_53_Data_Entry/main.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import requests
 from time import sleep
-from os import system
 
 
 chrome_options = Options()
@@ -14,8 +11,18 @@ chrome_options.add_experimental_option("detach", True)
 
 form_link = "https://docs.google.com/forms/d/e/1FAIpQLScJXp_yzZ6mOH7QtZUdub0mMvfyXyfdDCvtn4wZPEbF-hawqw/viewform?usp=sf_link"
 
-with open("index.html", "r") as index_html:
-    data = index_html.read()
+zillow_link = "https://www.zillow.com/san-francisco-ca/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22north%22%3A37.8834888362371%2C%22east%22%3A-122.23248568896484%2C%22south%22%3A37.666936495460185%2C%22west%22%3A-122.63417331103516%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A0%2C%22max%22%3A872627%7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A3000%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%7D"
+header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+    "Accept-Language": "en-US,en;q=0.5",
+}
+
+zillow_data = requests.get(url=zillow_link, headers=header)
+zillow_data.raise_for_status()
+data = zillow_data.text
+
+# with open("index.html", "w") as index:
+#     index.write(data)
 
 soup = BeautifulSoup(data, "html.parser")
 data_list = soup.find_all(
@@ -24,6 +31,7 @@ data_list = soup.find_all(
 )
 
 print(f"Total Property Found:- {len(data_list)}")
+print(*data_list, sep="\n")
 property_detail = []
 
 for fragment_list in data_list:
