5c26e1fbcad531a1eb26e2c108e84c4b830136d7
nico-zero <zandaxheart955@gmail.com>
2023-06-09 05:22:36 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_54_Hello_Flask/main.py
+++ New_Shit__/Angela_Yu__/Day_54_Hello_Flask/main.py
@@ -1,32 +1,32 @@
-# from flask import Flask
-# import heapq
+from flask import Flask
+import heapq
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-
-
-# if __name__ == "__main__":
-#     app.run()
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
-def add(a,b):
-    return a + b
+
+if __name__ == "__main__":
+    app.run()
 
 
-def subtract(a,b):
-    return a- b
+# def add(a,b):
+#     return a + b
 
-def divide(a,b):
-    return a/b
 
-def multiply (a,b):
-    return a*b
+# def subtract(a,b):
+#     return a- b
 
-def calculator (calculator_function, n1, n2):
-    return calculator_function(n1, n2)
+# def divide(a,b):
+#     return a/b
 
-print(calculator(add, 23,34))
+# def multiply (a,b):
+#     return a*b
+
+# def calculator (calculator_function, n1, n2):
+#     return calculator_function(n1, n2)
+
+# print(calculator(add, 23,34))
