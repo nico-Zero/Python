07920b15b683064b01150cd325441a9336c1d1e2
nico-zero <zandaxheart955@gmail.com>
2023-06-09 19:08:36 +0530

    Updating files

---
+++ New_Shit__/Angela_Yu__/Day_55_Flask_2/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import heapq
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!<p>"
+
+
+@app.route("/bye")
+def bye_world():
+    return "<p>Bye, World!<p>"
+
+if __name__ == "__main__":
+    app.run()
