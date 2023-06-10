da2d5dfccd3b05418affab6e4510533789e1f8f8
nico-zero <zandaxheart955@gmail.com>
2023-06-10 07:24:52 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_55_Flask_2/server.py
+++ New_Shit__/Angela_Yu__/Day_55_Flask_2/server.py
@@ -1,12 +1,42 @@
 from flask import Flask
-
+from random import randint
 
 app = Flask(__name__)
+lucky_number = randint(1,10)
+x = 0
+
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return "<h1>Guess the lucky Number between 1 and 10.</h1>" \
+            "<h1>And right the input in URL Bar.</h1>" \
+            "<img src='https://media1.giphy.com/media/sJz799uFb32es/giphy.webp?cid=ecf05e47b97mz4ptq75y2fiv0i1b7whbx8nu3q4v5lo8wj0g&ep=v1_gifs_search&rid=giphy.webp&ct=g' width='500px' hight='1000px'>"
+
+@app.route("/<int:number>")
+def high_low(number):
+    global x, lucky_number
+    if x >= 3:
+        x=1
+        lucky_number = randint(1,10)
+    else:
+        x += 1
+    
+    print(lucky_number)
+    if not 1 > number > 10:
+        return f"<h1>{number} FOR REAL NIGGER!!!</h1>" \
+            "<img src='https://media3.giphy.com/media/2NABZ6SkKNTGjD6D2D/200.webp?cid=ecf05e47l1db6o0lxqvdrn6o55laqq2uywclopuia9xlmkkl&ep=v1_gifs_search&rid=200.webp&ct=g' width='500px' hight='1000px'>"
+    elif number == lucky_number:
+        return f"<h1>Yes {number} is the lucky Number.</h1>" \
+            "<img src='https://media2.giphy.com/media/5exwXWg9u7yow/giphy.webp?cid=ecf05e47f6m1isnx40bg3locofjnscuj75w5tvdz0y4ehtsa&ep=v1_gifs_search&rid=giphy.webp&ct=g' width='500px' hight='1000px'>"
+    elif number < lucky_number:
+        return f"<h1>No {number} is lower then the lucky Number.</h1>" \
+            "<img src='https://media1.giphy.com/media/Ib6HtcZMKdPHaBj1R2/giphy.webp?cid=ecf05e471vx80lh1d1eakpkgv50b0e591s375ixvvzcpohh9&ep=v1_gifs_search&rid=giphy.webp&ct=g' width='500px' hight='1000px'>"
+    elif number > lucky_number:
+        return f"<h1>No {number} is higher then the lucky Number.</h1>" \
+            "<img src='https://media3.giphy.com/media/y9hjvnO2bwJbO/200.webp?cid=ecf05e476jrjf4358srip2820a4xuq1o4cmh6e4muyousbao&ep=v1_gifs_search&rid=200.webp&ct=g' width='500px' hight='1000px'>"
+    else:
+        return f"<h1>{number} FOR REAL NIGGER!!!</h1>" \
+            "<img src='https://media3.giphy.com/media/2NABZ6SkKNTGjD6D2D/200.webp?cid=ecf05e47l1db6o0lxqvdrn6o55laqq2uywclopuia9xlmkkl&ep=v1_gifs_search&rid=200.webp&ct=g' width='500px' hight='1000px'>"
 
 if __name__ == "__main__":
     app.run(debug=True)
-
