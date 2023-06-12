1a1cdceabc2cd70120b51a45d19bd71800d3b577
nico-zero <zandaxheart955@gmail.com>
2023-06-12 13:39:17 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_57_Flask_4/server.py
+++ New_Shit__/Angela_Yu__/Day_57_Flask_4/server.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
 from random import randint
 from datetime import date
+import requests
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     current_year = date.today().year
-    random_num = randint(1,100)
-    return render_template("index.html",random_number = random_num, year = current_year)
+    random_num = randint(1, 100)
+    return render_template("index.html", random_number=random_num, year=current_year)
+
+
+@app.route("guess/<name>")
+def guess(name: str):
+    gender = requests.get("https://api.genderize.io/?name=peter")
+    gender.raise_for_status()
+    gender = gender.json()
+
+    age = requests.get("https://api.agify.io/?name={name}")
+    age.raise_for_status()
+    age = age.json()
+
+    return render_template("guess.html", name=name.capitalize(), gender=gender, age=age)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
