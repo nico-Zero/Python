b436ef23ff08326c0edcb19177f218d6415e16c4
nico-zero <zandaxheart955@gmail.com>
2023-06-12 13:02:47 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_57_Flask_4/server.py
+++ New_Shit__/Angela_Yu__/Day_57_Flask_4/server.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from random import randint
+from datetime import date
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    current_year = date.today().year
+    random_num = randint(1,100)
+    return render_template("index.html",random_number = random_num, year = current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
