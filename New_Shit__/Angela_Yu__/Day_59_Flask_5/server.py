4a8345c5b40ba3d57c34467bb73103eb21baa9b2
nico-zero <zandaxheart955@gmail.com>
2023-06-16 08:58:47 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_59_Flask_5/server.py
+++ New_Shit__/Angela_Yu__/Day_59_Flask_5/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from datetime import date
 import requests
 import json
+
 
 app = Flask(__name__)
 
@@ -14,31 +16,44 @@ def home():
         data_file.write(json.dumps(blogs, indent=4))
 
     image = "../static/assets/img/home-bg.jpg"
+    year = date.today().year
 
-    return render_template("index.html", img=image, blog_posts=blogs)
+    return render_template("index.html", img=image, blog_posts=blogs, current_year=year)
 
 
 @app.route("/contact")
 def contact():
     image = "../static/assets/img/contact-bg.jpg"
-    return render_template("contact.html", img=image)
+    year = date.today().year
+
+    return render_template("contact.html", img=image, current_year=year)
 
 
 @app.route("/about")
 def about():
     image = "../static/assets/img/about-bg.jpg"
-    return render_template("about.html", img=image)
+    year = date.today().year
+
+    return render_template("about.html", img=image, current_year=year)
 
 
-@app.route("/post")
-def post():
+@app.route("/post/<int:blog_id>")
+def post(blog_id: int):
     with open("data.json") as data_file:
         blog_data = json.load(data_file)
-        blog = [post for post in blog_data if post["id"] == 1][0]
+        blog = [post for post in blog_data if post["id"] == blog_id][0]
 
     image = "../static/assets/img/post-bg.jpg"
+    year = date.today().year
 
-    return render_template("post.html", img=image, post=blog)
+    return render_template("post.html", img=image, post=blog, current_year=year)
+
+
+@app.route("/page/2")
+def page():
+    year = date.today().year
+
+    return render_template("page.html", current_year=year)
 
 
 if __name__ == "__main__":
