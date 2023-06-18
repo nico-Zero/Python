de38e7b86882ff970fa1660928695d0b037f9e9e
nico-zero <zandaxheart955@gmail.com>
2023-06-18 07:12:09 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_60_Flask_6/form_project/server.py
+++ New_Shit__/Angela_Yu__/Day_60_Flask_6/form_project/server.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/login')
-def login(password):
-    return password
 
-if __name__ == '__main__':
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template(
+        "login.html", user_name=request.form["name_"], password=request.form["password"]
+    )
+
+
+if __name__ == "__main__":
     app.run(debug=True)
