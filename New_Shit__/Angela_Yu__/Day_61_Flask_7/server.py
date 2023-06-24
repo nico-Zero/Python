364ac97af5102ca18e07b764149f02600165e0bc
nico-zero <zandaxheart955@gmail.com>
2023-06-25 01:45:05 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_61_Flask_7/server.py
+++ New_Shit__/Angela_Yu__/Day_61_Flask_7/server.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+# from flask import Flask, render_template, redirect
+# from flask_wtf import FlaskForm
+# from wtforms import StringField
+# from wtforms.validators import DataRequired
 
 
-class MyForm(FlaskForm):
-    name = StringField("name", validators=[DataRequired()])
+# class MyForm(FlaskForm):
+#     name = StringField("name", validators=[DataRequired()])
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    form = MyForm()
-    if form.validate_on_submit():
-        return redirect("/")
-    return render_template("index.html", form=form)
+# @app.route("/", methods=["GET", "POST"])
+# def home():
+#     form = MyForm()
+#     if form.validate_on_submit():
+#         return redirect("/")
+#     return render_template("index.html", form=form)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+import secrets
+
+
+print(dir(secrets))
