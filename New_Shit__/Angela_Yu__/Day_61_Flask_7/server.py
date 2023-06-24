64e18ed47da2786c1353f23468bf7afdb570c72f
nico-zero <zandaxheart955@gmail.com>
2023-06-25 02:19:37 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_61_Flask_7/server.py
+++ New_Shit__/Angela_Yu__/Day_61_Flask_7/server.py
@@ -1,27 +1,37 @@
-# from flask import Flask, render_template, redirect
-# from flask_wtf import FlaskForm
-# from wtforms import StringField
-# from wtforms.validators import DataRequired
-
-
-# class MyForm(FlaskForm):
-#     name = StringField("name", validators=[DataRequired()])
-
-
-# app = Flask(__name__)
-
-
-# @app.route("/", methods=["GET", "POST"])
-# def home():
-#     form = MyForm()
-#     if form.validate_on_submit():
-#         return redirect("/")
-#     return render_template("index.html", form=form)
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+from flask import Flask, render_template, redirect, request
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import secrets
 
+key = secrets.token_hex(64)
 
-print(dir(secrets))
+
+class MyForm(FlaskForm):
+    email = StringField("Email:- ", validators=[DataRequired()])
+    password = StringField("Password:- ", validators=[DataRequired()])
+    login = SubmitField("login")
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = key
+csrf = CSRFProtect(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    em_ps_form = MyForm()
+    if request.method == "POST":
+        if em_ps_form.validate_on_submit():
+            email = request.form["email"]
+            password = request.form["password"]
+            print(f"Email:- {email}, Password:- {password}")
+
+            return redirect("/")
+
+    return render_template("index.html", form=em_ps_form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
