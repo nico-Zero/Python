a172b7ffd3faca80b21dab8935bff6229e791342
nico-zero <zandaxheart955@gmail.com>
2023-06-25 01:37:15 +0530

    Updating files

--- New_Shit__/Angela_Yu__/Day_61_Flask_7/server.py
+++ New_Shit__/Angela_Yu__/Day_61_Flask_7/server.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class MyForm(FlaskForm):
+    name = StringField("name", validators=[DataRequired()])
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect("/")
     return render_template("index.html")
 
 
