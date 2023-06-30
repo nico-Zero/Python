f4d01eba2324aa7891fe7463ea1d2528bdb0d87a
nico-zero <zandaxheart955@gmail.com>
2023-06-30 18:19:07 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_63_/Bookself/BookSelf/main.py
+++ New_Shit__/Angela_Yu__/Day_63_/Bookself/BookSelf/main.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 
-
 class Input_form(FlaskForm):
-    name = StringField(label='Book Name:-', validators=[DataRequired()])
-    author = StringField(label='Book Author:-', validators=[DataRequired()])
-    rating = StringField(label='Rating:-', validators=[DataRequired()])
+    name = StringField(label="Book Name:-", validators=[DataRequired()])
+    author = StringField(label="Book Author:-", validators=[DataRequired()])
+    rating = SelectField(
+        label="Rating:-",
+        validators=[DataRequired()],
+        choices=("❌", "⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"),
+    )
     add = SubmitField("Add")
 
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap(app)
 
 all_books = []
@@ -20,13 +25,16 @@ all_books = []
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
     form = Input_form()
-
+    if form.validate_on_submit():
+        all_books.append({key: value for key, value in list(form.data.items())[:-2]})
+        print(all_books)
     return render_template("add.html", form=form)
 
 
