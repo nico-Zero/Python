7c72984da6aa21ca052bc5732addd3da598d3649
nico-zero <zandaxheart955@gmail.com>
2023-07-06 20:42:02 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_63_/Bookself/BookSelf/main.py
+++ New_Shit__/Angela_Yu__/Day_63_/Bookself/BookSelf/main.py
@@ -3,7 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
-import csv
+from csv import reader, writer
+
 
 class Input_form(FlaskForm):
     name = StringField(label="Book Name:-", validators=[DataRequired()])
@@ -20,25 +21,40 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap(app)
 
-all_books = []
-
 
 @app.route("/")
 def home():
-    
-    return render_template("index.html",library = bool(all_books) ,books=all_books)
+    all_books = []
+    with open("books.csv", newline="", encoding="utf8") as csv_file:
+        file = reader(csv_file, delimiter=",")
+        for row in file:
+            all_books.append(row)
+    parsed_books = [
+        {"name": name, "author": author, "rating": rating}
+        for name, author, rating in all_books[1:]
+    ]
+    return render_template("index.html", library=bool(all_books), books=parsed_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     form = Input_form()
     if form.validate_on_submit():
-        with open("books.csv", "+a") as csv_file:
-            ...
-        all_books.append({key: value for key, value in list(form.data.items())[:-2]})
-        print(all_books)
+        data = {key: value for key, value in list(form.data.items())[:-2]}
+        with open("books.csv", "+a", newline="", encoding="utf8") as csv_file:
+            file = writer(csv_file, delimiter=",")
+            file.writerow(data.values())
+
     return render_template("add.html", form=form)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# all_books = []
+# with open("books.csv", newline="", encoding="utf8") as csv_file:
+#     file = reader(csv_file, delimiter=",")
+#     for row in file:
+#         all_books.append(row)
+
+# print(all_books)
