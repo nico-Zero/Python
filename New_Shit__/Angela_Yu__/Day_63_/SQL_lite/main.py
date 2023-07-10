901e23fd0e912dc73719332b8940d54695c01570
nico-zero <zandaxheart955@gmail.com>
2023-07-10 08:54:24 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_63_/SQL_lite/main.py
+++ New_Shit__/Angela_Yu__/Day_63_/SQL_lite/main.py
@@ -1,10 +1,14 @@
-import sqlite3
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-db = sqlite3.connect("book_keeping.db")
-cursor = db.cursor()
 
-# cursor.execute(
-#     "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
-# )
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
