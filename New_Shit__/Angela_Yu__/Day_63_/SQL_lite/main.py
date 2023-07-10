c5a17ed2ed29fd00f38f1789bc59c69c6e58b1f0
nico-zero <zandaxheart955@gmail.com>
2023-07-10 08:59:59 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_63_/SQL_lite/main.py
+++ New_Shit__/Angela_Yu__/Day_63_/SQL_lite/main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///book_keeping.db"
+db.init_app(app)
 
 
 @app.route("/")
