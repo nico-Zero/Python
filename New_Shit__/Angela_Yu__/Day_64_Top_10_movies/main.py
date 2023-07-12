c9eb000eaca4e353459d2e6aa107abf11e421a9e
nico-zero <zandaxheart955@gmail.com>
2023-07-12 22:19:18 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_64_Top_10_movies/main.py
+++ New_Shit__/Angela_Yu__/Day_64_Top_10_movies/main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, FloatField, URLField
+from wtforms.validators import DataRequired, NumberRange
 import requests
 
 db = SQLAlchemy()
@@ -28,13 +28,45 @@ class Movies(db.Model):
 with app.app_context():
     db.create_all()
 
-class AddMovies():
+
+class AddMovies(FlaskForm):
     name = StringField(label="Movie Name:- ", validators=[DataRequired()])
+    year = StringField(label="Year:- ", validators=[DataRequired()])
+    rating = FloatField(
+        label="Rating:- ",
+        validators=[
+            DataRequired(),
+            NumberRange(min=0, max=10, message="Rating Must be 0-10."),
+        ],
+    )
+    image_url = URLField(label="Image URL:- ", validators=[DataRequired()])
+    description = StringField(label="Description:- ", validators=[DataRequired()])
+    comment = StringField(label="Comment:- ", validators=[DataRequired()])
+    add = SubmitField(label="Add")
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movies():
+    form = AddMovies()
+    if form.validate_on_submit():
+        ...
+
+    return render_template("add.html", form=form)
+
+
+@app.route("/update", methods=["GET", "POST"])
+def update_movie():
+    return render_template("update.html")
+
+
+@app.route("/delete")
+def delete_movie():
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
