8124405bebb47ea798585b6ec693340b4f5a1e3e
nico-zero <zandaxheart955@gmail.com>
2023-07-12 18:50:30 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_64_Top_10_movies/main.py
+++ New_Shit__/Angela_Yu__/Day_64_Top_10_movies/main.py
@@ -6,9 +6,27 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
+db = SQLAlchemy()
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap5(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+db.init_app(app)
+
+
+class Movies:
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    year = db.Column(db.String(5), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(1000), nullable=True, unique=True)
+    description = db.Column(db.String(1000), nullable=True)
+    comment = db.Column(db.String(1000), nullable=True)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
