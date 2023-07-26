8fe66cb85f2eff9fcb691feaf6856f1fa0b7dec5
nico-zero <zandaxheart955@gmail.com>
2023-07-26 22:09:37 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_67_Blog_Capstone_project_part_3/main.py
+++ New_Shit__/Angela_Yu__/Day_67_Blog_Capstone_project_part_3/main.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -9,13 +10,14 @@ from datetime import date
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap5(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
 db = SQLAlchemy()
 db.init_app(app)
+
 
 ##CONFIGURE TABLE
 class BlogPost(db.Model):
@@ -26,6 +28,9 @@ class BlogPost(db.Model):
     body = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
+
+    def to_dict(self):
+        return {i.name: getattr(self, i.name) for i in self.__table__.columns}
 
 
 with app.app_context():
@@ -38,18 +43,20 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = 'TODO: Add a CKEditorField'
+    body = "TODO: Add a CKEditorField"
     submit = SubmitField("Submit Post")
 
 
-@app.route('/')
+@app.route("/")
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
-    posts = []
+    data = list(db.session.query(BlogPost))
+    posts = [i.to_dict() for i in data]
+    print(posts)
     return render_template("index.html", all_posts=posts)
 
+
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route("/")
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
     requested_post = "Grab the post from your database"
