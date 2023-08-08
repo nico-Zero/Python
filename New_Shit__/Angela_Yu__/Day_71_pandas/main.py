b36ad44cf1f8bb0ec5d44aef6d0fb9ac0275912a
nico-zero <zandaxheart955@gmail.com>
2023-08-08 14:28:39 +0530

    updating

--- New_Shit__/Angela_Yu__/Day_71_pandas/main.py
+++ New_Shit__/Angela_Yu__/Day_71_pandas/main.py
@@ -4,11 +4,13 @@ from selenium.webdriver.chrome.options import Options
 
 
 chrome_options = Options()
-chrome_options.add_experimental_option("detach", False)
+chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-data = driver.get(
+driver.get(
     "https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors"
 )
+
+driver.find_elements(By.CLASS_NAME,"data-table__row")
 
