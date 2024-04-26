8b725f26259802586cf24da8d87e7a5bd2f9c098
Nico-Zero <zandaxheart955@gmail.com>
2024-04-26 22:53:03 +0530

    i arch linux nowclear!

---
+++ Data_science_python/06-Trees/b_heap.py
@@ -0,0 +1,51 @@
+from random import randint
+
+
+# def generate_heap_list(l, difference):
+#     heap_list = [0] + ([None] * (2**l))
+#     l = 2 ** (l - 1)
+#     r = 1
+#     for i in range(1, l + randint(1, l + 1)):
+#         heap_list[i] = randint(r, r + difference)
+#         r += difference
+#     return list(heap_list)
+
+
+# heap_list = generate_heap_list(5, 15)
+# for i, j in enumerate(heap_list):
+#     print("{:<5}{:<5}".format(str(i), str(j)))
+
+
+class binHeap:
+    def __init__(self):
+        self.heaplist = [0]
+        self.currentSize = 0
+
+    def __perUp(self, i):
+        while i // 2 > 0:
+            if self.heaplist[i] < self.heaplist[i // 2]:
+                self.heaplist[i], self.heaplist[i // 2] = (
+                    self.heaplist[i // 2],
+                    self.heaplist[i],
+                )
+            i = i // 2
+
+    def insert(self, key):
+        self.heaplist.append(key)
+        self.currentSize += 1
+        self.__perUp(self.currentSize)
+
+    def insertFromList(self, l):
+        for key in l:
+            self.heaplist.append(key)
+            self.currentSize += 1
+            
+
+
+x = binHeap()
+for _ in range(10):
+    x.insert(randint(1, 100))
+
+print(x.heaplist)
+x._binHeap__perUp(x.currentSize) #type: ignore
+print(x.heaplist)
