3e0801ccae07ead3c943f8c441669bb10f1aad8c
Nico-Zero <zandaxheart955@gmail.com>
2024-05-01 04:00:52 +0530

    storing data

--- Data_science_python/06-Trees/b_heap.py
+++ Data_science_python/06-Trees/b_heap.py
@@ -1,25 +1,25 @@
 from random import randint
 
 
-# def generate_heap_list(l, difference):
-#     heap_list = [0] + ([None] * (2**l))
-#     l = 2 ** (l - 1)
-#     r = 1
-#     for i in range(1, l + randint(1, l + 1)):
-#         heap_list[i] = randint(r, r + difference)
-#         r += difference
-#     return list(heap_list)
+def generate_heap_list(l, difference):
+    heap_list = [0] + ([None] * (2**l))
+    l = 2 ** (l - 1)
+    r = 1
+    for i in range(1, l + randint(1, l + 1)):
+        heap_list[i] = randint(r, r + difference)
+        r += difference
+    return list(heap_list)
 
 
-# heap_list = generate_heap_list(5, 15)
-# for i, j in enumerate(heap_list):
-#     print("{:<5}{:<5}".format(str(i), str(j)))
+heap_list = generate_heap_list(5, 15)
+for i, j in enumerate(heap_list):
+    print("{:<5}{:<5}".format(str(i), str(j)))
 
 
 class binHeap:
-    def __init__(self):
-        self.heaplist = [0]
-        self.currentSize = 0
+    def __init__(self, heap_list=[]):
+        self.heaplist = heap_list
+        self.currentSize = len(self.heaplist) - 1
 
     def __perUp(self, i):
         while i // 2 > 0:
@@ -31,14 +31,19 @@ class binHeap:
             i = i // 2
 
     def insert(self, key):
-        self.heaplist.append(key)
-        self.currentSize += 1
-        self.__perUp(self.currentSize)
-
-    def insertFromList(self, l):
-        for key in l:
+        if self.heaplist[-1] != None:
             self.heaplist.append(key)
-            self.currentSize += 1
+        else:
+            for i in range(self.currentSize + 1)[::-1]:
+                if self.heaplist[i - 1] != None:
+                    self.heaplist.insert(i, key)
+                    break
+        self.currentSize += 1
+        # print(self.heaplist[i])
+        if self.heaplist[-1] != None:
+            self.__perUp(self.currentSize)
+        else:
+            self.__perUp(i)
 
     def __perDown(self, i):
         while i * 2 <= self.currentSize:
@@ -68,10 +73,10 @@ class binHeap:
         return retval
 
 
-x = binHeap()
-for _ in range(10):
-    x.insert(randint(1, 100))
+x = binHeap(heap_list)
+# for _ in range(10):
+#     x.insert(randint(1, 100))
 
 print(x.heaplist)
-x._binHeap__perUp(x.currentSize)  # type: ignore
+x.insert(200)
 print(x.heaplist)
