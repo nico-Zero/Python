20e503f805b11ba7f6b4f3ee72062b8b63fc3153
Nico-Zero <zandaxheart955@gmail.com>
2024-06-30 13:16:36 +0530

    Update.

--- mechine_learn/5_machine_learning.py
+++ mechine_learn/5_machine_learning.py
@@ -1,34 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# print("hello, World !!!")
-
 def f(x):
     return x**2 + x + 1
 
 # make data
 x_1 = np.linspace(start=-3, stop=3, num=100)
-[print(x) for x in x_1]
+# [print(x) for x in x_1]
 # x = (x for x in x_1)
 # print( next(x) )
 
-plt.plot(x_1, f(x_1))
-plt.xlabel("x", fontsize=12)
-plt.ylabel("f(x)", fontsize=12)
+# plt.plot(x_1, f(x_1))
+# plt.xlabel("x", fontsize=12)
+# plt.ylabel("f(x)", fontsize=12)
 # plt.show()
 
 
-# Slope & Deritive of Function.
+## Slope & Deritive of Function.
 def df(x):
     return 2*x + 1
 
 
-# Plot Function and deritive side by side.
+## Plot Function and deritive side by side.
 
 plt.figure(figsize=(10,5))
+
+## Plot 1
+
+plt.subplot(1,2,1)
 
 plt.plot(x_1, f(x_1))
 plt.xlabel("x", fontsize=12)
 plt.ylabel("f(x)", fontsize=12)
-plt.show()
 
+## Plot 2
+plt.subplot(1,2,2)
+
+plt.plot(x_1, df(x_1))
+plt.xlabel("x", fontsize=12)
+plt.ylabel("f(x)", fontsize=12)
+
+plt.show()
